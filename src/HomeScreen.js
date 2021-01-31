cb49ee5a4ad8ab37690a9b1034d6e9f7e44e0d71
Zito Namuro <45439075+pottozito@users.noreply.github.com>
2021-01-30 19:55:24 -0500

    Fix spelling

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -18,12 +18,12 @@ const HomeScreen = () => {
                             <img src={portrait} />
                         </div>
                         <div className={'mb-5'} style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
-                            <motion.a href={'https://github.com/pottozito'} initial={{ opacity: 0 }} animate={{ y: [10, 0], opacity: [0, 1] }} transition={{ duration: 0.4, delay: 0.6 }} className={'px-3'}>Github <FontAwesomeIcon size='xs' icon={faChevronRight} /></motion.a>
-                            <motion.a href={'https://www.linkedin.com/in/pottozito/'} initial={{ opacity: 0 }} animate={{ y: [10, 0], opacity: [0, 1] }} transition={{ duration: 0.4, delay: 0.8 }} className={'px-3'}>Linkedin <FontAwesomeIcon size='xs' icon={faChevronRight} /></motion.a>
+                            <motion.a href={'https://github.com/pottozito'} initial={{ opacity: 0 }} animate={{ y: [10, 0], opacity: [0, 1] }} transition={{ duration: 0.4, delay: 0.6 }} className={'px-3'}>GitHub <FontAwesomeIcon size='xs' icon={faChevronRight} /></motion.a>
+                            <motion.a href={'https://www.linkedin.com/in/pottozito/'} initial={{ opacity: 0 }} animate={{ y: [10, 0], opacity: [0, 1] }} transition={{ duration: 0.4, delay: 0.8 }} className={'px-3'}>LinkedIn <FontAwesomeIcon size='xs' icon={faChevronRight} /></motion.a>
                             <motion.a href={'mailto:namuro@uwindsor.ca'} initial={{ opacity: 0 }} animate={{ y: [10, 0], opacity: [0, 1] }} transition={{ duration: 0.4, delay: 1.0 }} className={'px-3'}>Email <FontAwesomeIcon size='xs' icon={faChevronRight} /></motion.a>
                         </div>
                         <div>
-                            <h6 className={'mb-3'}>Recent HighLights</h6>
+                            <h6 className={'mb-3'}>Recent Highlights</h6>
                             <HighLights />
                         </div>
                     </motion.div>
